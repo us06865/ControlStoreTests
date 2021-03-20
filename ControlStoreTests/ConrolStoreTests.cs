adf470dca5eac5b85d40c2157a09046e1f06f2b5
CaseyQ76@gmail.com <CaseyQ76@gmail.com>
2021-03-20 10:34:15 -0700

    Add project files.

---
+++ ControlStoreTests/ConrolStoreTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System.Threading;
+using ControlStoreTestsCore;
+
+namespace ControlStoreTests
+{
+    public class ConrolStoreTests
+    {
+        [TestFixture]
+        public class ControlStoreTests
+        {
+            Core controlStoreCore;
+            /// set up runs before each test OneTimeSetUp runs when class is instantiated
+            [SetUp]
+            public void Setup()
+            {
+                controlStoreCore = new Core();
+                controlStoreCore.Initialize("file:///C:/Users/Casey/source/repos/ControlStore.html");
+
+            }
+            [TearDown]
+            public void TearDown()
+            {
+                controlStoreCore.Terminate();
+            }
+            [Test]
+            public void VerifyCheckBoxesSelection()
+            {
+
+                controlStoreCore.Click(controlStoreCore.CheckBoxesButton);
+                Thread.Sleep(1000);
+                controlStoreCore.Click("checkbox1");
+                controlStoreCore.Click("checkbox3");
+                controlStoreCore.Click(controlStoreCore.CheckBoxUpdateButton);
+                string textFound=controlStoreCore.GetTextFromElement(controlStoreCore.CheckboxSelectedLabel);
+                Assert.AreEqual("Checked 13", textFound, "CheckboxSelectedLabel does not display expected text.");
+                Thread.Sleep(5000);
+            }
+            [Test]
+            public void RadioButtonSelection()
+            {
+                controlStoreCore.Click(controlStoreCore.RadioButtonsButton);
+                Thread.Sleep(1000);
+                controlStoreCore.Click("radio1");
+                controlStoreCore.Click(controlStoreCore.RadioButtonUpdate);
+                controlStoreCore.Click("radio123Label");
+                string textFound = controlStoreCore.GetTextFromElement(controlStoreCore.RadioSelectedLabel);
+                Assert.AreEqual("Selected 1", textFound, "RadioSelectedLabel does not display expected text.");
+                Thread.Sleep(5000);
+            }
+            [Test]
+            public void DropdownsComboBoxSelection()
+            {
+                // IWebElement element = driver.FindElementByXPath("/html/ body / nav / ul / li[3] / button");
+                controlStoreCore.Click(controlStoreCore.ComboBoxesButton);
+                controlStoreCore.Click("combo1");
+                //element = driver.FindElementByXPath("//*[@id=\"combo1\"]/option[3]");
+                controlStoreCore.Click(controlStoreCore.ComboBoxOption3);
+                // element = driver.FindElementByXPath("//*[@id=\"dropdownArticle\"]/section/ul/li[2]/button");
+                controlStoreCore.Click(controlStoreCore.ComboBoxButtonUpdate);
+                controlStoreCore.Click("comboLabel");
+                string textFound = controlStoreCore.GetTextFromElement(controlStoreCore.ComboBoxLabel);
+                Assert.AreEqual("Selected 2", textFound,"ComboBoxLabel does not display expected text.");
+                Thread.Sleep(5000);
+
+            }
+        }
+    }
+}
---
+++ ControlStoreTestsCore/Core.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace ControlStoreTestsCore
+{
+    public class Core
+    {
+        ChromeDriver driver;
+        private Dictionary<string, By> Identifiers;
+
+        #region Identifier Strings 
+        #region Checkboxes 
+        public string CheckBoxesButton = "html/body/nav/ul/li[1]/button";
+        public string CheckBox1Id = "checkbox1";
+        public string CheckBox2Id = "checkbox2";
+        public string CheckBox3Id = "checkbox3";
+        public string CheckBoxUpdateButton = "//*[@id=\"checkboxesArticle\"]/section/ul/li[4]/button";
+        public string CheckboxSelectedLabel = "checkboxLabel";
+        #endregion
+        #region Radiobuttons
+        public string RadioButtonsButton = "/html/body/nav/ul/li[2]/button";
+        public string RadioButton1 = "radio1";
+        public string RadioButtonUpdate = "//*[@id=\"radioButtonsArticle\"]/section/ul/li[4]/button";
+        public string RadioSelectedLabel = "radio123Label";
+        #endregion
+        #region ComboBoxes
+        public string ComboBoxesButton = "/html/body/nav/ul/li[3]/button";
+        public string ComboBox1Id = "combo1";
+        public string ComboBoxOption3 = "//*[@id=\"combo1\"]/option[3]";
+        public string ComboBoxButtonUpdate="/html/body/page/article[3]/section/ul/li[2]/button";
+        public string ComboBoxLabel = "comboLabel";
+
+        #endregion
+        #endregion
+
+        public void Initialize(string startingUrl)
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(startingUrl);
+            Identifiers = new Dictionary<string, By>();
+            Identifiers.Add(CheckBoxesButton, By.XPath(CheckBoxesButton));
+            Identifiers.Add(CheckBox1Id, By.Id(CheckBox1Id));
+            Identifiers.Add(CheckBox2Id, By.Id(CheckBox2Id));
+            Identifiers.Add(CheckBox3Id, By.Id(CheckBox3Id));
+            Identifiers.Add(CheckBoxUpdateButton,By.XPath(CheckBoxUpdateButton));
+            Identifiers.Add(CheckboxSelectedLabel,By.Id(CheckboxSelectedLabel));
+            Identifiers.Add(RadioButtonsButton, By.XPath(RadioButtonsButton));
+            Identifiers.Add(RadioButton1, By.Id(RadioButton1));
+            Identifiers.Add(RadioButtonUpdate, By.XPath(RadioButtonUpdate));
+            Identifiers.Add(RadioSelectedLabel, By.Id(RadioSelectedLabel));
+            Identifiers.Add(ComboBoxesButton, By.XPath(ComboBoxesButton));
+            Identifiers.Add(ComboBox1Id, By.Id(ComboBox1Id));
+            Identifiers.Add(ComboBoxOption3, By.XPath(ComboBoxOption3));
+            Identifiers.Add(ComboBoxButtonUpdate, By.XPath(ComboBoxButtonUpdate));
+            Identifiers.Add(ComboBoxLabel, By.Id(ComboBoxLabel));
+
+
+        }
+
+        public void Click(string locator)
+        {
+            By elementIdentifier = Identifiers[locator];
+            try
+            {
+                driver.FindElement(elementIdentifier).Click();
+
+            }
+            catch (NotFoundException)
+            {
+                throw new Exception("Cound not find element using the ID" + locator
+                    + "Review the identifier to see if it is correct");
+            }
+        }
+
+        public string GetTextFromElement(string locator)
+        {
+            By elementIdentifier = Identifiers[locator];
+            string textFound = string.Empty;
+            textFound = driver.FindElement(elementIdentifier).Text;
+            return textFound;
+
+        }
+        public void Terminate()
+        {
+            driver.Quit();
+        }
+    }
+}
